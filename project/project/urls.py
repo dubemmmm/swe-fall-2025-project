f8473057a3cf6415359204bfdb98f338d1185452
cherisha2003 <109971941+cherisha2003@users.noreply.github.com>
2025-10-27 19:03:19 -0400

    Add adoption URLs to urlpatterns

--- project/project/urls.py
+++ project/project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', user_views.home, name='home'),
     path('users/', include('users.urls')),
+    path('adoption/', include('adoption.urls', namespace='adoption')),
 ]
 
 # Serve media files in development
